045af2e120ace5d95b43d0ac5fdcdc75dc8f88bc
cdj2073 <79151181+cdj2073@users.noreply.github.com>
2023-07-05 22:22:28 +0900

    comment: Add comment

--- src/main/java/com/example/learnspringframework/AppGamingBasicJava.java
+++ src/main/java/com/example/learnspringframework/AppGamingBasicJava.java
@@ -6,7 +6,11 @@ public class AppGamingBasicJava {
     public static void main(String[] args) {
         // var game = new MarioGame();
         // var game = new SuperContraGame();
+
+        // 1. Object Creation
         var game = new PacmanGame();
+        // 2. Object Creation + Wiring of Dependencies
+        //     Game is a Dependency of GameRunner
         var gameRunner = new GameRunner(game);
         gameRunner.run();
     }
