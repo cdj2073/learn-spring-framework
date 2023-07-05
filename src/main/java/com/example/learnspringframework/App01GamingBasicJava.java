86a1f9cffc2959b159b600c09206e43436a3ff56
cdj2073 <79151181+cdj2073@users.noreply.github.com>
2023-07-05 22:26:19 +0900

    rename: AppGamingBasicJava -> App01GamingBasicJava

--- src/main/java/com/example/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/example/learnspringframework/App01GamingBasicJava.java
@@ -2,7 +2,7 @@ package com.example.learnspringframework;
 
 import com.example.learnspringframework.game.*;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
     public static void main(String[] args) {
         // var game = new MarioGame();
         // var game = new SuperContraGame();
